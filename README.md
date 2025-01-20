2069a2955307971f30284314dcb3ee1c9a99d7af
Abhilash Patil <89824888+abhilash15500@users.noreply.github.com>
2025-01-20 15:48:22 +0530

    write html css code for the weather app

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # odin-weather-app
+
+bgimg - https://unsplash.com/photos/silhouette-photo-of-person-standing-rMmibFe4czY
+
+https://unsplash.com/photos/time-lapse-photography-of-stars-iafcRHii_1I
--- src/index.js
+++ src/index.js
@@ -1,1 +1,28 @@
 import "./styles.css";
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// toggle switch
+
+const toggleSwitch = document.getElementById("toggle");
+
+// Optional: Add event listener to display the state
+toggleSwitch.addEventListener("change", () => {
+  const state = toggleSwitch.checked ? "F" : "C";
+  console.log("Current state:", state); // For debugging
+});
